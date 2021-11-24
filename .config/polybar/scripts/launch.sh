496866b1de1283b4670ca9388d618bb76b6f1643
Furkan Begen <furkanbegen@gmail.com>
2021-11-25 02:01:42 +0300

    Changes config to use multi monitor setup

--- .config/polybar/scripts/launch.sh
+++ .config/polybar/scripts/launch.sh
@@ -8,5 +8,6 @@ killall -q polybar
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 polybar top 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar bottom 2>&1 | tee -a /tmp/polybar1.log & disown
 
 echo "Bars launched..."
--- scripts/both.sh
+++ scripts/both.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-xrandr --output DP-0 --off --output DP-1 --off --output HDMI-0 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP-2 --off --output eDP-1-1 --mode 2560x1440 --pos 3840x0 --rotate normal
+xrandr --output DP-0 --off --output DP-1 --off \
+  --output HDMI-0 --primary --mode 2560x1440 --pos 0x0 --rotate normal \
+  --output DP-2 --off --output eDP-1-1 --mode 2560x1440 --pos 3840x0 --rotate normal --below HDMI-0
+
