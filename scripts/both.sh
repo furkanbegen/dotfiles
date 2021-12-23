9ac152eac4e5217c2b2f7362602e245a3510fb0b
Furkan Begen <furkanbegen@gmail.com>
2021-12-24 01:25:05 +0300

    Adds PRIME Synchronization to the monitor scripts

--- scripts/both.sh
+++ scripts/both.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 xrandr --output DP-0 --off --output DP-1 --off \
   --output HDMI-0 --primary --mode 2560x1440 --pos 0x0 --rotate normal \
-  --output DP-2 --off --output eDP-1-1 --mode 1920x1080 --pos 3840x0 --rotate normal --below HDMI-0
+  --output DP-2 --off --output eDP-1-1 --set "PRIME Synchronization" 1 \
+  --mode 1920x1080 --pos 3840x0 --rotate normal --below HDMI-0
 
