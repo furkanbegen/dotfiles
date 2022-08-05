c52ebb27d3bef3851d264854830f1864d4c8ea60
Furkan Begen <furkanbegen@gmail.com>
2022-08-06 02:33:18 +0300

    Changes moniyor related scripts

--- scripts/hotplug.sh
+++ scripts/hotplug.sh
@@ -3,18 +3,19 @@
 # Get out of town if something errors
 set -e
 
-extern=HDMI-0
+extern=DP-0
 
 if xrandr | grep "$extern disconnected"; then
-  xrandr --output HDMI-0 --off --output eDP-1-1 --primary --mode 2560x1440
+  xrandr --output DP-0 --off --output eDP-1-1 --primary --mode 2560x1440 \
+    --scale 0.6x0.6
 	notify-send --urgency=low -t 5000 "Graphics Update" "External monitor disconnected"	
 else 
   
-  xrandr --output DP-0 --off --output DP-1 --off \
-  --output HDMI-0 --primary --mode 3840x2160 --pos 0x0 --rotate normal \
+  xrandr --output HDMI-0 --off --output DP-1 --off \
+  --output DP-0 --primary --mode 2560x1440 --pos 0x0 --rotate normal \
   --output DP-2 --off --output eDP-1-1 --set "PRIME Synchronization" 1 \
-  --mode 2560x1440 --scale 1.2x1.2 --pos 3840x0 --rotate normal --below HDMI-0
-  notify-send --urgency=low -t 5000 "Graphics Update" "HDMI plugged in"
+  --mode 2560x1440 --scale 0.6x0.6 --pos 3840x0 --rotate normal --below DP-0
+  notify-send --urgency=low -t 5000 "Graphics Update" "Display Port plugged in"
 	exit
 fi
 
--- scripts/internal.sh
+++ scripts/internal.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-xrandr --output HDMI-0 --off --output eDP-1-1 --primary --mode 2560x1440 --scale 1.2x1.2
+xrandr --output DP-0 --off --output eDP-1-1 --primary --mode 2560x1440 --scale 1.2x1.2
 
