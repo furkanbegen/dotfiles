ebf322c4753b324001cd6610914208cd44c58462
Furkan Begen <furkanbegen@gmail.com>
2022-08-06 18:36:47 +0300

    Adds scale option to xrandr scipts

--- scripts/internal.sh
+++ scripts/internal.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-xrandr --output DP-0 --off --output eDP-1-1 --primary --mode 2560x1440 --scale 1.2x1.2
+xrandr --output DP-0 --off --output eDP-1-1 --primary --mode 2560x1440 --scale 0.6x0.6
 
