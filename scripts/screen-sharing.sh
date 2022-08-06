f468e4f84a7603eb3db71dc91ed7bbd712699480
Furkan Begen <furkanbegen@gmail.com>
2022-08-06 17:03:34 +0300

    Updades xrandr scripts

--- scripts/screen-sharing.sh
+++ scripts/screen-sharing.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-
-xrandr --output DP-0 --off --output DP-1 --off \
-  --output DP-0 --primary --mode 1920x1080 --pos 0x0 --rotate normal \
-  --output DP-2 --off --output eDP-1-1 --set "PRIME Synchronization" 1 \
-  --mode 2560x1440 --scale 0.6x0.6 --below DP-0
+xrandr --output DP-0 --primary --mode 1920x1080 --pos 0x0 \
+  --rotate normal --output DP-1 --off --output HDMI-0 --off \
+  --output DP-2 --off --output eDP-1-1 --mode 2560x1440 \
+  --pos 0x1080 --rotate normal
